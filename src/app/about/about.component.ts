07a66ce91ab103d238836864ce93b117faeb814a
Jacob R Drawhorn <87413802+JacobDrawhorn@users.noreply.github.com>
2022-08-20 04:24:09 -0700

    update

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -9,8 +9,8 @@ export class AboutComponent implements OnInit {
 
   constructor() { }
   title: string = "Welcome to my website ";
-  paragraph: string = "I'm a full stack developer, Portland based. I have experience working on a multitude of projects in a "
-                      + "variety of areas within software develpment. ";
+  paragraph: string = "I'm a full stack developer, Portland based. I have experience working on a multitude of projects "
+                      + "within full stack web develpment. I'm driven, passionate, and love to learn. "
   ngOnInit() {}
 
 }
